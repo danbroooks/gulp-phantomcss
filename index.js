37a559b89217768d0624dab2626ab8552ee2e2a8
Dan Brooks <danger.dan_@hotmail.co.uk>
2014-09-08 23:35:09 +0100

    Fleshed out core of plugin.

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 'use strict';
 
+var _ = require('lodash');
+var path = require('path');
+var spawn = require('child_process').spawn;
 var gutil = require('gulp-util');
 var through = require('through2');
 
-var path = require('path');
-var phantomBinary = require('phantomjs').path;
-var runnerPath = path.join(__dirname, '..', 'phantomjs', 'runner.js');
-var phantomCSSPath = path.join(__dirname, '..', 'bower_components', 'phantomcss');
+var runnerjs = path.join(__dirname, 'runner.js');
+var phantomcss = path.join(__dirname, 'node_modules', 'phantomcss');
 
-module.exports = function (options) {
-  if (!options) throw new gutil.PluginError('gulp-phantomcss', 'Missing options');
+var defaults = _.partialRight(_.assign, function(a, b) {
+  return typeof a == 'undefined' ? b : a;
+});
 
-  if (typeof options === 'string') {
-    var configFile = options;
-    options = {
-      configFile: configFile
-    };
-  }
+function spawnPhantom(args){
+  var o = {
+    stdio: 'inherit',
+    cwd: process.cwd()
+  };
+  return spawn(require('phantomjs').path, args, o);
+};
 
-        // exec('phpunit <%= file.path %>')
+module.exports = function (opts) {
+
+  opts = _.extend(opts || {}, {
+    screenshots: 'screenshots',
+    results: 'results',
+    viewportSize: [1280, 800],
+    logLevel: 'error'
+  });
+
+  opts.phantomCSSPath = phantomcss;
+  opts.test = [];
 
   return through.obj(function (file, enc, cb) {
-    console.log('first');
+    opts.test.push(path.resolve(file.path));
     cb(null, file);
-  }, function(cb) {
-    console.log('second');
+  }, function(cb){
+    var phantom = spawnPhantom([
+      runnerjs,
+      JSON.stringify(opts)
+    ]);
   });
 
 };
