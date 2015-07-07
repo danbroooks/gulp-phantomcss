7f2f3e4ad572d0db4b7e816ca269b35b0b8e248c
danbroooks <danger.dan_@hotmail.co.uk>
2015-07-07 22:30:51 +0100

    Move runner.js into src/

--- src/paths.js
+++ src/paths.js
@@ -5,7 +5,7 @@ var paths = {};
 
 paths.root = path.join(__dirname, '..');
 
-paths.runnerjs = path.join(paths.root, 'runner.js');
+paths.runnerjs = path.join(paths.root, 'src', 'runner.js');
 paths.phantomcss = path.join(paths.root, 'node_modules', 'phantomcss');
 
 module.exports = paths;
