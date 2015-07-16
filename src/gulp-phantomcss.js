b2b4b282591a0a3e4eac42e7d3acdb7b25ec260e
Paul Jeter <pajeter@cisco.com>
2015-07-16 16:26:59 -0500

    Fix for issue #2
    Fixes #2 Test failure does not exit gulp task with correct status.
    Add support for more setting more native PhantomCSS options.
    Update README.md with updated options.
    Left previous option names as fallback for backward compatability.

--- src/gulp-phantomcss.js
+++ src/gulp-phantomcss.js
@@ -1,7 +1,8 @@
-
 var _ = require('lodash');
 var path = require('path');
 var through = require('through2');
+var gutil = require('gulp-util');
+var PluginError = gutil.PluginError;
 
 module.exports.configure = function(config) {
   this.paths = config.paths;
@@ -9,7 +10,7 @@ module.exports.configure = function(config) {
   return this;
 };
 
-module.exports.through = function(args){
+module.exports.through = function(args) {
   args = _.extend({
     screenshots: 'screenshots',
     results: 'results',
@@ -19,15 +20,31 @@ module.exports.through = function(args){
 
   args.paths = this.paths;
   var phantom = this.phantom;
+  var error = 0;
+
+  function endStream() {
+    if (args.breakOnError && error > 0) {
+      return this.emit('error', new PluginError(
+          'gulp-phantomcss', 'Some tests have failed.'
+      ));
+    }
+  }
 
   var stream = through.obj(function(file, enc, cb) {
+    if (file.isStream()) {
+      return this.emit('error', new PluginError(
+          'gulp-phantomcss',  'Streaming not supported'
+      ));
+    }
     args.test = path.resolve(file.path);
 
     phantom(args.paths.runnerjs, args)
-      .on('exit', function(){
-          cb(null, file);
+      .on('exit', function(fail) {
+        if (fail) {
+          error++;
+        }
+        cb(null, file);
       });
-  });
-
+  }, endStream);
   return stream;
 };
