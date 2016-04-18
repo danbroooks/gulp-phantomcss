7a3ce7d6ff3a08339fbd1c1d7696c1851e76f124
Soren Juul <sorenjuul@gmail.com>
2016-04-18 10:34:45 +0200

    Added other path detection strategy

--- src/paths.js
+++ src/paths.js
@@ -1,21 +1,29 @@
-
 var path = require('path');
+var fs = require('fs');
 
 var root = path.join(__dirname, '..');
+var parentNodeModules = path.join(root, '..');
+var rootNodeModules = path.join(root, 'node_modules');
+
+var currentNodeModules;
+try {
+    if (fs.lstatSync(path.join(rootNodeModules, 'phantomcss')).isDirectory()) {
+        currentNodeModules = rootNodeModules;
+    }
+} catch (e) {
+    currentNodeModules = parentNodeModules;
+}
 
 var phantomjs = require('phantomjs-prebuilt');
 var phantomjsBinPath = phantomjs.path;
 
-var getInstalledPath = require('get-installed-path');
-
 var paths = {
     root: root,
     runnerjs: path.join(root, 'src', 'runner.js'),
-    phantomjs: phantomjsBinPath,
-    phantomcss: getInstalledPath('phantomcss', true)//path.join(root, 'node_modules', 'phantomcss')
+    phantomjs:  phantomjsBinPath,
+    phantomcss: path.join(currentNodeModules, 'phantomcss'),
+    casper: path.join(currentNodeModules, 'casperjs')
 };
-
-paths.casper = getInstalledPath('casperjs', true);//path.join(paths.phantomcss, 'node_modules', 'casperjs');
 
 var bin = {
     casper: path.join(paths.casper, 'bin')
